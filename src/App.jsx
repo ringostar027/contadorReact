4daa54e4cd6f4f927317dc594b11e44673a49d97
dev2 <rlopdav392@g.educaand.es>
2025-10-28 18:43:00 +0100

    nueva versión dev2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Contador from "./components/Contador";
+import Header from "./components/Header";
 function App() {
   return (
     <div>
       <h1>Señor contador</h1>
+      <Header />
       <Contador />
     </div>
   );
