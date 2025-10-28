985690f2a3a367e78ad0c1b54aa77761c4ca0d01
dev2 <rlopdav392@g.educaand.es>
2025-10-28 13:19:31 +0100

    movida dev2

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -3,13 +3,7 @@ function Contador() {
   const [contador, setContador] = useState(0);
 
   function increment() {
-    /*  setContador((contadorPrevious) => contadorPrevious + 1);
-     */
-    /*  setContador((contadorPrevious) => contadorPrevious + 1);
-    setContador((contadorPrevious) => contadorPrevious + 1); */
-    /*Setter es asíncrono, ya que se ejecute cuando a él le parece la forma más <eficiente*></eficiente*/
-    /*Y hablando de eficiencia, react es tan eficiente,que no es necesario usar delegación de eventos*/
-    /*El usa su propio mecanismo de "delegación de eventos interno"*/
+    console.log("movida dev2");
 
     setContador((contadorPrevious) => contadorPrevious + 1);
     console.log("valor actual de contador", contador);
